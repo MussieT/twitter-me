c40d7da7c68f97b2806852fe8e7b2b9631324570
Mussie Teshome <Mussieteshomeg@gmail.com>
2022-11-28 01:02:07 +0300

    Header component

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types'
+import AddTaskButton from './AddTask'
+
+// Bookmarks reminder, chrome extension
+
+const Header = ({ title }) => {
+    const onClick = () => {
+        console.log('button is clicked!');
+    }
+
+    return (
+        <header className="flex justify-between">
+            <h1 data-testid="header-h1" className='text-3xl font-bold'>{title}</h1>
+            <AddTaskButton text="Add Task" onClick={onClick} />
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title: "Task Tracker"
+}
+
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
+}
+
+export default Header
