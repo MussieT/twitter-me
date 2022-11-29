4ff73b0c683f38611fdf31ccc44061e3228e98cd
Mussie Teshome <Mussieteshomeg@gmail.com>
2022-11-29 07:31:39 +0300

    New component - Add task

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,21 +1,48 @@
-import PropTypes from 'prop-types'
+import Button from './Button'
+import { useState } from 'react'
 
-const AddTaskButton = ({ text, className, onClick }) => {
+const AddTask = ({ onAdd }) => {
+    const [text, setText] = useState('')
+    const [day, setDay] = useState('')
+    const [reminder, setReminder] = useState(false)
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        if(!text) {
+            alert('please add text')
+            return
+        }
+
+        onAdd({text, day, reminder})
+
+        setText('')
+        setDay('')
+        setReminder(false)
+    }
+
     return (
-        <button data-testid="addtask-button"
-            className={className} onClick={onClick}>{text}</button>
+        <form className="form w-full grid grid-cols-6 gap-0" onSubmit={onSubmit}>
+            <div className="task-inputs-grid">
+                <label className="task-label">Task</label>
+                <input className="task-input" type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Add Task" />
+            </div>
+            <div className="task-inputs-grid">
+                <label className="task-label mt-3">Day & Time</label>
+                <input className="task-input" type="text" value={day} onChange={(e) => setDay(e.target.value)} placeholder="Add Day & time" />
+            </div>
+            <div className="task-inputs-grid">
+                <div className="w-1/2 flex justify-between">
+                    <label className="task-label mt-3">Set Reminder</label>
+                    <input type="checkbox" className="mt-1 w-5" value={reminder} 
+                        onChange={(e) => setReminder(e.currentTarget.checked)} placeholder="Add Day & time" />
+                </div>
+            </div>
+            <div className="col-span-5 col-start-2 my-2">
+                <Button text="Save Task" type="submit" className="w-1/2 bg-black text-white rounded-lg px-10 py-2" />
+            </div>
+        </form>
     )
 }
 
-AddTaskButton.defaultProps = {
-    text: "Add",
-    className: "text-xl rounded-xl bg-black px-4 py-2 text-white"
-}
-
-AddTaskButton.propTypes = {
-    text: PropTypes.string,
-    className: PropTypes.string,
-    onClick: PropTypes.func
-}
-
-export default AddTaskButton
+export default AddTask
