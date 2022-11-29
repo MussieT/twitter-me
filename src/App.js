0c954f6c1862f76b5d711742e9ebbfa7f90b9fd2
Mussie Teshome <Mussieteshomeg@gmail.com>
2022-11-29 07:33:52 +0300

    use state with app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,58 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask"
 
 function App() {
+  const [ ThingsToDo, setThingsToDo ] = useState(
+    [
+        {
+            id: '1',
+            text: 'Doctors Appointment',
+            day: 'Feb 5th 2023',
+            reminder: true
+        },
+        {
+            id: '2',
+            text: 'Meeting at School',
+            day: 'Jan 11 2023',
+            reminder: false
+        },
+        {
+          id: '3',
+          text: 'Shopping',
+          day: 'Jan 14 2023',
+          reminder: false
+        }
+    ]
+  )
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setThingsToDo(ThingsToDo.filter((task) => task.id != id))
+  }
+
+  // Toggle reminder
+  const toggleReminder = (id) => {
+    setThingsToDo(
+      ThingsToDo.map((task) => task.id === id ? { ...task, reminder: !task.reminder} : task)
+    )
+  }
+
+  // Add Task
+  const onAdd = (task) => {
+    // console.log('obj: ', object)
+    const id = Math.ceil(Math.random() * 1000) + 1
+    const newThing = { id, ...task }
+    setThingsToDo([...ThingsToDo, newThing]) 
+  }
+
   return (
-    <div  className='mt-10 mx-auto border border-black w-1/2 p-5 rounded-xl justify-between'>
-        <div className="block">
+    <div  className='mt-10 mx-auto border border-black w-1/2 p-5 rounded-xl'>
           <Header title="Task Tracker" />
-        </div>
-        <div className="block">
-          <Tasks />
-        </div>
+          <AddTask onAdd={onAdd} />
+          {ThingsToDo.length ? <Tasks ThingsToDo={ThingsToDo}  
+              onDelete={deleteTask} onToggle={toggleReminder}  /> : 'No Tasks to show'}
     </div>
   );
 }
